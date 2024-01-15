6f57eb57c012c6cf2a910e74a4f0ee9b22c2fe57
Steve Yang <steveyangwork@gmail.com>
2024-01-15 10:15:58 -0600

    setup for eventform.tsx

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,11 +1,18 @@
 import EventForm from '@/components/shared/EventForm';
+import { getEventById } from '@/lib/actions/event.actions';
 import { auth } from '@clerk/nextjs';
 
-const UpdateEvent = () => {
-	// hook from nextjs
+type UpdateEventProps = {
+	params: {
+		id: string;
+	};
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
 	const { sessionClaims } = auth();
 
 	const userId = sessionClaims?.userId as string;
+	const event = await getEventById(id);
 
 	return (
 		<>
@@ -16,7 +23,12 @@ const UpdateEvent = () => {
 			</section>
 
 			<div className="wrapper my-8">
-				<EventForm userId={userId} type="Update" />
+				<EventForm
+					type="Update"
+					event={event}
+					eventId={event._id}
+					userId={userId}
+				/>
 			</div>
 		</>
 	);
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,5 +1,23 @@
-// this form will have events
+// this form will have keyboard/tap events so 'use client' is required
 'use client';
+
+// 1. import zodResolver, useForm, z
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+// 5. import Form... components
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { eventDefaultValues } from '@/constants';
+import { eventFormSchema } from '@/lib/validator';
 
 type EventFormProps = {
 	userId: string;
@@ -7,10 +25,66 @@ type EventFormProps = {
 };
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+	// 7. import `eventDefaultValues` from constants/index.ts
+	const initialValues = eventDefaultValues;
+
+	// 3. define form
+	const form = useForm<z.infer<typeof eventFormSchema>>({
+		resolver: zodResolver(eventFormSchema),
+		// 8. set default values
+		defaultValues: initialValues,
+	});
+
+	// 4. Define a submit handler.
+	function onSubmit(values: z.infer<typeof eventFormSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+	// 6. Return <Form />
 	return (
-		<div>
-			UserId: {userId}, EventForm {type}
-		</div>
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex flex-col gap-5">
+				<div className="flex flex-col gap-5 md:flex-row">
+					<FormField
+						control={form.control}
+						name="title"
+						render={({ field }) => (
+							<FormItem className="w-full">
+								<FormControl>
+									<Input
+										placeholder="Event Title"
+										className="input-field"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="categoryId"
+						render={({ field }) => (
+							<FormItem className="w-full">
+								<FormControl>
+									{/* <Input
+										placeholder="Event Title"
+										className="input-field"
+										{...field}
+									/> */}
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
 	);
 };
 
